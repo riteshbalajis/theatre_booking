021192d1d7c10a7448aa79011ce39c42ffd8b846
Ritesh Balaji S <riteshbalajis@gmail.com>
2026-09-08 15:24:30 +0530

    Update DBConnection.java

--- src/main/com/movie_booking/util/DBConnection.java
+++ src/main/com/movie_booking/util/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public final class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/movie_booking"
             + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" ;
-    private static final String USER = getEnvironmentValue("MOVIE_DB_USER", "root");
-    private static final String PASSWORD = getEnvironmentValue("MOVIE_DB_PASSWORD", "Sql1234@");
+    private static final String USER = getEnvironmentValue("MOVIE_DB_USER", "****");
+    private static final String PASSWORD = getEnvironmentValue("MOVIE_DB_PASSWORD", "****");
 
     private DBConnection() {
         // Utility class.
